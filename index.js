f5f66497d1abe67fe9657892b085236df6ded5d1
amjad019926 <67501289+amjad019926@users.noreply.github.com>
2025-07-29 01:54:58 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { google } = require('googleapis');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+
+const auth = new google.auth.GoogleAuth({
+  keyFile: 'service-account.json',
+  scopes: ['https://www.googleapis.com/auth/androidpublisher']
+});
+
+app.post('/check', async (req, res) => {
+  try {
+    const { packageName, productId, purchaseToken } = req.body;
+    const client = await auth.getClient();
+    const androidpublisher = google.androidpublisher({ version: 'v3', auth: client });
+
+    const result = await androidpublisher.purchases.products.get({
+      packageName,
+      productId,
+      token: purchaseToken
+    });
+
+    res.json({ status: result.data.purchaseState });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(3000, () => console.log("API is running on port 3000"));
