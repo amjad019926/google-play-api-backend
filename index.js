cf80ca784aaa9116a2106f048785aabe961d71a4
amjad019926 <67501289+amjad019926@users.noreply.github.com>
2026-01-03 15:16:38 +0600

    Update index.js

--- index.js
+++ index.js
@@ -5,25 +5,37 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+/**
+ * Google Auth
+ */
 const auth = new google.auth.GoogleAuth({
   keyFile: '/etc/secrets/service-account.json',
   scopes: ['https://www.googleapis.com/auth/androidpublisher']
 });
 
+/**
+ * ORDER LOOKUP BY ORDER ID (NEW SYSTEM)
+ */
 app.post('/order', async (req, res) => {
   try {
     const { packageName, orderId } = req.body;
 
     if (!packageName || !orderId) {
-      return res.status(400).json({ error: 'packageName and orderId required' });
+      return res.status(400).json({
+        error: 'packageName and orderId are required'
+      });
     }
 
+    // Get access token
     const client = await auth.getClient();
     const accessToken = await client.getAccessToken();
 
-    const url = `https://androidpublisher.googleapis.com/androidpublisher/v3/applications/${packageName}/orders/${orderId}`;
+    // Orders REST API (SDK does NOT support this yet)
+    const url =
+      `https://androidpublisher.googleapis.com/androidpublisher/v3/applications/${packageName}/orders/${orderId}`;
 
     const response = await fetch(url, {
+      method: 'GET',
       headers: {
         Authorization: `Bearer ${accessToken.token}`
       }
@@ -35,7 +47,7 @@ app.post('/order', async (req, res) => {
       return res.status(response.status).json(data);
     }
 
-    const item = data.lineItems?.[0] || {};
+    const item = data.lineItems && data.lineItems[0] ? data.lineItems[0] : {};
 
     res.json({
       orderId: data.orderId,
@@ -53,6 +65,10 @@ app.post('/order', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Order API running on port 3000');
+/**
+ * REQUIRED FOR RENDER
+ */
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
