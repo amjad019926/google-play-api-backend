088b51d8e48399414b735cdaabb553b905e68102
amjad019926 <67501289+amjad019926@users.noreply.github.com>
2026-01-03 15:04:37 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { google } = require('googleapis');
 const bodyParser = require('body-parser');
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const app = express();
 app.use(bodyParser.json());
@@ -19,15 +18,12 @@ app.post('/order', async (req, res) => {
       return res.status(400).json({ error: 'packageName and orderId required' });
     }
 
-    // 🔑 Get OAuth access token
     const client = await auth.getClient();
     const accessToken = await client.getAccessToken();
 
-    // 📡 Orders REST API call
     const url = `https://androidpublisher.googleapis.com/androidpublisher/v3/applications/${packageName}/orders/${orderId}`;
 
     const response = await fetch(url, {
-      method: 'GET',
       headers: {
         Authorization: `Bearer ${accessToken.token}`
       }
@@ -39,16 +35,16 @@ app.post('/order', async (req, res) => {
       return res.status(response.status).json(data);
     }
 
-    const lineItem = data.lineItems?.[0] || {};
+    const item = data.lineItems?.[0] || {};
 
     res.json({
       orderId: data.orderId,
-      productId: lineItem.productId || null,
-      purchaseToken: lineItem.purchaseToken || null,
-      purchaseTimeMillis: lineItem.purchaseTimeMillis || null,
-      state: lineItem.state || null,
-      priceMicros: lineItem.price?.amountMicros || null,
-      currency: lineItem.price?.currencyCode || null
+      productId: item.productId || null,
+      purchaseToken: item.purchaseToken || null,
+      purchaseTimeMillis: item.purchaseTimeMillis || null,
+      state: item.state || null,
+      priceMicros: item.price?.amountMicros || null,
+      currency: item.price?.currencyCode || null
     });
 
   } catch (err) {
@@ -57,4 +53,6 @@ app.post('/order', async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log('Order API running'));
+app.listen(3000, () => {
+  console.log('Order API running on port 3000');
+});
